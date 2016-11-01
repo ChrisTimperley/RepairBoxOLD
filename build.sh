2c823e7aae48aef923999af492660e191e4f3994
Chris Timperley <christimperley@googlemail.com>
2016-11-01 13:43:47 -0400

    updated Dockerfiles and build file

--- build.sh
+++ build.sh
@@ -2,3 +2,7 @@
 #
 # (Re-)builds all of the RepairBox docker images, skipping any whose
 # Dockerfile hasn't changed.
+
+# build base images
+docker build -t repairbox:fedora64 base/fedora64
+docker build -t repairbox:ubuntu32 base/ubuntu32
